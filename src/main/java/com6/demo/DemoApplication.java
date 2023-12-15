ae6046e2024f02eb46735edccf35c21fc79f88fa
Yuyang Li (Ian) <923109530@qq.com>
2023-12-15 16:59:24 -0500

    Fixed Id return

--- src/main/java/com6/demo/DemoApplication.java
+++ src/main/java/com6/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com6.demo;
 
+import com6.demo.entity.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Product.class);
 	}
 
 }
